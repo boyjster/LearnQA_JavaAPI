358da5139732fd010bf888e0dcd8bd24774b80b1
boyjster <boyjster@gmail.com>
2023-11-13 07:46:42 +0300

    second lesson

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,14 +1,40 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
-        response.prettyPrint();
+    public void testRestAssured(){
+        Map<String, String> data = new HashMap<>();
+        data.put("login", "secret_login");
+        data.put("password", "secret_passl");
+
+         Response responseForGet = RestAssured
+                 .given()
+                 .body(data)
+                 .when()
+                 .post("https://playground.learnqa.ru/api/get_auth_cookie")
+                 .andReturn();
+
+         String responseCookie = responseForGet.getCookie("auth_cookie");
+
+         Map<String, String> cookies = new HashMap<>();
+         if(responseCookie != null){
+             cookies.put("auth_cookie", responseCookie);
+         }
+
+         Response responseForCheck = RestAssured
+                 .given()
+                 .body(data)
+                 .cookies(cookies)
+                 .when()
+                 .post("https://playground.learnqa.ru/api/check_auth_cookie")
+                 .andReturn();
+         responseForCheck.print();
     }
 }
