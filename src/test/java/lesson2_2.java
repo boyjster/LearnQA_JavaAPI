5cc8fd4076a6c86dc057c51391bce198752c0e08
boyjster <boyjster@gmail.com>
2023-11-14 21:58:58 +0300

    second lesson

--- src/test/java/lesson2_2.java
+++ src/test/java/lesson2_2.java
@@ -1,15 +1,27 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.http.client.protocol.ResponseContentEncoding;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class lesson2_2 {
     @Test
-    public void testPunktTwo(){
+    public void testPunktTwo() {
 
-        JsonPath responseJSON = RestAssured
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
                 .get("https://playground.learnqa.ru/api/long_redirect")
-                .jsonPath();
+                .andReturn();
 
-        System.out.println(responseJSON.prettyPrint())
+        String header = response.getHeader("Location");
+        System.out.println("Redirect URL: " + header);
 
+    }
 }
