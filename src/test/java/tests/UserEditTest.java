b9042332c2e895b1670d9f6ee39ee6328a7fc5f0
boyjster <boyjster@gmail.com>
2023-11-29 00:36:50 +0300

    third lesson

---
+++ src/test/java/tests/UserEditTest.java
@@ -0,0 +1,68 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import lib.DataGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserEditTest extends BaseTestCase {
+    @Test
+    public void testEditJustCreatedTes(){
+        //GENERATE USER
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+
+        //System.out.println(userData);
+        JsonPath responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .jsonPath();
+
+        String userId = responseCreateAuth.getString("id");
+        //System.out.println("userId " + userId);
+
+        //LOGIN
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", userData.get("email"));
+        authData.put("password", userData.get("password"));
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+        //System.out.println("server answer: " + responseGetAuth.asString());
+
+        //EDIT
+        String newName = "Changed Name";
+        Map<String, String> editData = new HashMap<>();
+        editData.put("firstName", newName);
+
+        Response responseEditUser = RestAssured
+                .given()
+                .header("x-csrf-token", this.getHeader(responseGetAuth, "x-csrf-token"))
+                .cookie("auth_sid", this.getCookie(responseGetAuth, "auth_sid"))
+                .body(editData)
+                .put("https://playground.learnqa.ru/api/user/" + userId)
+                .andReturn();
+
+
+        //GET
+        Response responseUserData = RestAssured
+                .given()
+                .header("x-csrf-token", this.getHeader(responseGetAuth, "x-csrf-token"))
+                .cookie("auth_sid", this.getCookie(responseGetAuth, "auth_sid"))
+                .get("https://playground.learnqa.ru/api/user/" + userId)
+                .andReturn();
+
+        //System.out.println(responseUserData.asString());
+        Assertions.asserJsonByName(responseUserData, "firstName", newName);
+    }
+}
